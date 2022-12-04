cbcee4b5412ca943d59112acc3ef588cc77a87ee
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-04 18:20:26 +0100

    uploade first folder cshap

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        // Einstiegspunkt für das Programm
+        static void Main(string[] args)
+        {
+            // Setzt die Hintergrundfarbe der Konsole
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            // Setzt die Schriftfarbe der Konsole
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            // Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Hallo Denis");
+            Console.ReadKey();
+            
+            /*
+             * Ich bin ein
+             * sehr
+             * langer Kommentar
+             **/
+        }
+    }
+
+}
