22b26346dda6069e0c34dfd918ff749b308aa75d
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-07 17:26:06 +0100

    change sturctur for me

---
+++ me-code/KonsolHelloJeff/KonsolHelloJeff/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        // Einstiegspunkt für das Programm
+        static void Main(string[] args)
+        {
+            // Setzt die Hintergrundfarbe der Konsole
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            // Setzt die Schriftfarbe der Konsole
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            // Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Hallo Jeff");
+            Console.ReadKey();
+
+            /*
+             * Ich bin ein
+             * sehr
+             * langer Kommentar
+             **/
+        }
+    }
+
+}
